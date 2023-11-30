42e118927cc42b4c05d06f238eb8bb52485b6a7c
Sem Jansen <1076101@hr.nl>
2023-11-30 11:56:59 +0100

    Begin van inlog en register + aanpassingen in main.py

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, redirect, request, url_for
 from flask import request
+from wtforms import Form, StringField
+import sqlite3
 
 app = Flask(__name__)
 
+# Locatie van het database bestand
+# database_file = "databases/testgpt.db"
+# # Maak verbinding met het database bestand
+# conn = sqlite3.connect(database_file)   
+# # Maak een cursor object waarmee je SQL statements kan uitvoeren
+# c = conn.cursor()
+# # Voer een SQL statement uit
+# result = c.execute("SELECT count(*) FROM teachers")
+# # Nu niet nodig, maar stel dat dit een UPDATE statement was, dan had je nu moeten committen
+# # conn.commit()
+# number_of_teachers = result.fetchone()[0]  
+# print(f"Er zijn {number_of_teachers} docenten in de database")
+# # Sluit de verbinding met de database, is netjes, moet niet
+# conn.close()
+
+# class RegisterForm(Flaskform):
+    # username = StringField(validators=[InputRequired(), Length(min=4, max=20)])
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
-
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 @app.route('/login')
 def login():
@@ -21,10 +42,12 @@ def login():
 def login_check():
     username = request.form['username']
     password = request.form['password']
-    return username
+
+
+    return redirect(url_for('app.index'))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
 
 
